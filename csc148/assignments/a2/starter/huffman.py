db3b9c86c3749346c6fbd0f3c177d99666c7b8b2
KunalMehta4 <159668500+KunalMehta4@users.noreply.github.com>
2026-06-09 09:37:38 -0400

    Update huffman.py

--- csc148/assignments/a2/starter/huffman.py
+++ csc148/assignments/a2/starter/huffman.py
@@ -2,15 +2,6 @@
 Assignment 2 
 CSC148
 Instructors: Bogdan Simion, Rutwa Engineer, Marc De Benedetti, Romina Piunno
-
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) Bogdan Simion, Dan Zingaro
 """
 from __future__ import annotations
 
